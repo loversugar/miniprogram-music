e4200ffe3d4ab0061fffeb22795bd7089d5a3f27
椰子是只猫 <tangxuelei@yezishizhimaodeMacBook-Pro.local>
2020-08-04 07:41:17 +0800

    添加自定义组件playlist，修改样式

--- miniprogram/components/playlist/playlist.js
+++ miniprogram/components/playlist/playlist.js
@@ -1,13 +1,18 @@
 Component({
   /**
    * 组建初始化数据
+   * 组件内部数据
    */
   data: {},
 
   /**
    * 组件的属性列表
+   * 传递过来值
    */
   properties: {
+    playlist: {
+      type: Object
+    }
   },
 
   /**
