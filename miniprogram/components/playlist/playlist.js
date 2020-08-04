9362387fcff1f583ab2232558e31dda310dea0f3
椰子是只猫 <tangxuelei@yezishizhimaodeMacBook-Pro.local>
2020-08-04 08:17:34 +0800

    修改播放量显示，设置wx:key

--- miniprogram/components/playlist/playlist.js
+++ miniprogram/components/playlist/playlist.js
@@ -3,7 +3,9 @@ Component({
    * 组建初始化数据
    * 组件内部数据
    */
-  data: {},
+  data: {
+    _count: 0
+  },
 
   /**
    * 组件的属性列表
@@ -15,8 +17,34 @@ Component({
     }
   },
 
+  // 进行监听
+  observers: {
+    ['playlist.playCount'](count) {
+      // 如果监听某个值的时候，不能再对监听的值进行赋值，这样
+      // 会形成死循环，因为监听值改变，会再次出发监听
+      // 解决方案就是重新定义一个新的值
+      this.setData({
+        _count: this._tranNumber(count, 2)
+      })
+    }
+  },
+
   /**
    * 组件的方法列表
    */
-  methods: {}
+  methods: {
+    _tranNumber(num, point) {
+      let numStr = num.toString().split('.')[0]
+      if (numStr.length < 6) {
+        return numStr
+      } else if (numStr.length >= 6 && numStr.length <= 8) {
+        let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + point) 
+        
+        return parseFloat(parseInt(num /10000) + '.' + decimal) + '万'
+      } else if (numStr.length > 8) {
+        let decimal = numStr.substring(numStr.length - 8, numStr.length - 8 + point)
+        return parseFloat(parseInt(num / 100000000) + '.' + decimal) + '亿'
+      }
+    }
+  }
 })
