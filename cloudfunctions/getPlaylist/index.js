0bd45e1d2b3df7db1373e11b39f9334dcc47ab02
loversugar <253262275@qq.com>
2020-08-05 08:19:26 +0800

    添加歌单详情页面

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -11,10 +11,31 @@ const URL = 'http://musicapi.xiecheng.live/personalized'
 
 const playlistCollection = db.collection('playlist')
 
+const MAX_LIMIT = 100
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   // 云函数从数据库获取数据只能获取100条，小程序端获取数据只能获取20条
-  const list =  await playlistCollection.get()
+  // const list =  await playlistCollection.get()
+
+  const countResult = await playlistCollection.count()
+  const total = countResult.total
+  const batchTimes = Math.ceil(total / MAX_LIMIT)
+  const tasks = []
+  for (let i=0; i<batchTimes; i++) {
+    let promise = playlistCollection.skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+    tasks.push(promise)
+  }
+  let list = {
+    data: []
+  }
+  if (tasks.length > 0) {
+    list =  (await Promise.all(tasks)).reduce((acc, cur) => {
+      return {
+        data: acc.data.concat(cur.data)
+      }
+    })
+  }
 
   const playlist = await rp(URL).then((res) => {
     return JSON.parse(res).result
