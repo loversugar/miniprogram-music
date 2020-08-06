7067c7aed0468f3e299dbbc160701ac8fa00fbc0
loversugar <253262275@qq.com>
2020-08-06 08:09:21 +0800

    添加音乐播放fonticon

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -2,13 +2,17 @@
 let musiclist = []
 //正在播放歌曲的index
 let nowPlayingIndex = 0 
+
+// 获取全局唯一的背景音频管理器(相当于单例模式)
+const backgroundAudioManager = wx.getBackgroundAudioManager();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    picUrl: ''
+    picUrl: '',
+    isPlaying: false, // false表示不播放，true表示正在播放
   },
 
   /**
@@ -18,12 +22,12 @@ Page({
     console.log(options)
     nowPlayingIndex = options.index
     musiclist = wx.getStorageSync('musiclist');
-    this._loadMusicDetail()
+    this._loadMusicDetail(options.musicid)
   },
 
-  _loadMusicDetail() {
+  _loadMusicDetail(musicId) {
     let music = musiclist[nowPlayingIndex]
-    console.log(music)
+    console.log(musicId)
     wx.setNavigationBarTitle({
       title: music.name,
       success: (result)=>{
@@ -34,8 +38,45 @@ Page({
     });
 
     this.setData({
-      picUrl: music.al.picUrl
+      picUrl: music.al.picUrl,
+      isPlaying: false
     })
+
+    wx.showLoading({
+      title: "歌曲加载中",
+      mask: true,
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+
+    wx.cloud.callFunction({
+      name: 'music',
+      data: {
+        musicId,
+        $url: 'musicUrl',
+        // es6新语法
+        // musicId: musicId
+      }
+    }).then((res) => {
+      console.log(res)
+      let result = JSON.parse(res.result)
+      backgroundAudioManager.title = music.name
+      backgroundAudioManager.src = result.data[0].url
+      backgroundAudioManager.coverImgUrl = music.al.picUrl
+      backgroundAudioManager.singer = music.ar[0].name
+      backgroundAudioManager.epname = music.al.name
+
+      this.setData({
+        isPlaying: true
+      })
+    })
+
+    console.log("hhhaha" + this.data.isPlaying)
+
+    wx.hideLoading();
   },
 
   /**
