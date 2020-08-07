a4840ac4800953cc60299721fcc08dc948b29972
loversugar <253262275@qq.com>
2020-08-07 08:20:57 +0800

    添加进度条

---
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -0,0 +1,12 @@
+Component({
+  data: {
+    showTime: {
+      currentTime: '00:00',
+      totalTime: '00:00'
+    },
+    movableDis: 0,
+    progress: 0
+  },
+  properties: {},
+  methods: {}
+})
