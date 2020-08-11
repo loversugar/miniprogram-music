c5bf68645f5e8bdf81e063d96f6caeebe9e9b2e6
loversugar <253262275@qq.com>
2020-08-11 22:15:23 +0800

    完成我的布局

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,4 +1,28 @@
+// 1.场景 只想在界面上显示自己的头像，别人的得不到
+// <open-data type="userAvatarUrl"></open-data>
+// <open-data type="userNickName"></open-data>
+// <open-data type="userCountry"></open-data>
+// <open-data type="userCity"></open-data>
+
+// 2. 获取用户信息,可以将用户信息存储到数据
+// 前提用户必须授权才可以
+// wx.getUserInfo()
+
+// 3. 由用户主动决定是否授权
+// <button open-type="getUserInfo" bindgetUserInfo="onGetUserInfo"></button
+
+// 4.如果想使用openid
+// 传统方式
+// 步骤  1. 小程序调用wx.login获取code 2.调用wx.getRequest将code传递给后端服务器
+// 3.后端服务器拿到code访问微信服务器换取opendid和session_key
+// 4.发送到小程序端
+
+// 云开发模式
+// 步骤 1.用户点击获取用户信息按钮 2.小程序调用云函数获取用户信息 3.返回用户信息
+// 小程序将用户信息存储到云数据库
+
 // pages/profile/profile.js
+
 Page({
 
   /**
